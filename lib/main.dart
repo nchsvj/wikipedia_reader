c08946853e1d78343ead95e15b66a4e0041690f7
nchsvj <kalimaga12@gmail.com>
2026-05-15 11:59:25 +0500

    min fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'ui/article_view/widgets/articlePage_screen.dart';
-import 'ui/article_view/cubit/article_cubit.dart';
-import 'ui/main_menu/main_menu.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'ui/article_view/widgets/articlePage_screen.dart';
+// import 'ui/article_view/cubit/article_cubit.dart';
+// import 'ui/main_menu/main_menu.dart';
 import 'ui/core/app_router.dart';
 
 void main() {
--- lib/ui/main_menu/main_menu.dart
+++ lib/ui/main_menu/main_menu.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:wikipedia_reader/ui/article_view/widgets/articlePage_screen.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wikipedia_reader/ui/article_view/cubit/article_cubit.dart';
+// import 'package:wikipedia_reader/ui/article_view/widgets/articlePage_screen.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:wikipedia_reader/ui/article_view/cubit/article_cubit.dart';
 
 class MainMenu extends StatelessWidget {
   const MainMenu({super.key});
